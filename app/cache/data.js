2dafbd4d6672a78048ac55e7fa1ca62a5653c710
AidenTeal <112907438+AidenTeal@users.noreply.github.com>
2024-07-11 11:44:03 -0600

    Create data.js

---
+++ app/cache/data.js
@@ -0,0 +1,112 @@
+export const playerData = {
+    global: {
+      name: 'poopsmear123',
+      tag: '',
+      uid: '2535437048996428',
+      avatar: null,
+      platform: 'X1',
+      level: 224,
+      toNextLevelPercent: 33,
+      internalUpdateCount: 3318,
+      bans: {
+        isActive: false,
+        remainingSeconds: 0,
+        last_banReason: 'COMPETITIVE_DODGE_COOLDOWN'
+      },
+      rank: {
+        rankScore: 0,
+        rankName: 'Unranked',
+        rankDiv: 0,
+        ladderPosPlatform: -1,
+        rankImg: 'https://api.mozambiquehe.re/assets/ranks/unranked4.png',
+        rankedSeason: 'br_ranked',
+        ALStopPercent: 'No game this split',
+        ALStopInt: 'No game this split',
+        ALStopPercentGlobal: 'No game this split',
+        ALStopIntGlobal: 'No game this split',
+        ALSFlag: true
+      },
+      arena: {
+        rankScore: 0,
+        rankName: 'Unranked',
+        rankDiv: 0,
+        ladderPosPlatform: -1,
+        rankImg: 'https://api.mozambiquehe.re/assets/ranks/unranked4.png',
+        rankedSeason: 'arenas21_split_2',
+        ALStopPercent: 'No game this split',
+        ALStopInt: 'No game this split',
+        ALStopPercentGlobal: 'No game this split',
+        ALStopIntGlobal: 'No game this split',
+        ALSFlag: true
+      },
+      battlepass: { level: null, history: null },
+      internalParsingVersion: 2,
+      badges: [{ /* Object content */ }],
+      levelPrestige: 0
+    },
+    realtime: {
+      lobbyState: 'open',
+      isOnline: 0,
+      isInGame: 0,
+      canJoin: 0,
+      partyFull: 0,
+      selectedLegend: 'Bangalore',
+      currentState: 'offline',
+      currentStateSinceTimestamp: -1,
+      currentStateAsText: 'Offline'
+    },
+    legends: {
+      selected: {
+        LegendName: 'Bangalore',
+        data: [/* Array content */],
+        gameInfo: { /* Object content */ },
+        ImgAssets: { /* Object content */ }
+      },
+      all: {
+        Global: { /* Object content */ },
+        Revenant: { /* Object content */ },
+        Crypto: { /* Object content */ },
+        Horizon: { /* Object content */ },
+        Gibraltar: { /* Object content */ },
+        Wattson: { /* Object content */ },
+        Fuse: { /* Object content */ },
+        Bangalore: { data: [{
+            value: 675,
+            rank: {
+                rankPos: 743278
+            },
+            rankPlatformSpecific: {
+                rankPos: 106003
+            }
+        }] },
+        Wraith: { /* Object content */ },
+        Octane: { /* Object content */ },
+        Bloodhound: { /* Object content */ },
+        Caustic: { /* Object content */ },
+        Lifeline: { /* Object content */ },
+        Pathfinder: { /* Object content */ },
+        Loba: { /* Object content */ },
+        Mirage: { /* Object content */ },
+        Rampart: { /* Object content */ },
+        Valkyrie: { /* Object content */ },
+        Seer: { /* Object content */ },
+        Ash: { /* Object content */ },
+        'Mad Maggie': { /* Object content */ },
+        Newcastle: { /* Object content */ },
+        Vantage: { /* Object content */ },
+        Catalyst: { /* Object content */ },
+        Ballistic: { /* Object content */ },
+        Conduit: { /* Object content */ },
+        Alter: { /* Object content */ }
+      }
+    },
+    mozambiquehere_internal: { isNewToDB: false, clusterSrv: 'fr-1' },
+    total: {
+      kills: { name: 'BR Kills', value: 1346 },
+      damage: { name: 'BR Damage', value: 289590 },
+      kd: { value: '-1', name: 'KD' }
+    },
+    processingTime: 0.2507050037384033
+  };
+
+  
